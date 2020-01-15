94c9016e26decdd6299ca362b96839707ea64c9c
themafia98 <gaara33377@gmail.com>
2020-01-15 23:36:25 +0300

    config init

--- PHP-ReactJS-todo/api/config/appConfig.php
+++ PHP-ReactJS-todo/api/config/appConfig.php
@@ -1,6 +1,21 @@
 <?php
 
-namespace config\app;
-/** App config */
+/**
+ * App config file
+ */
+namespace config\app\env;
+
+use Dotenv\Dotenv;
+
+require  "../vendor/autoload.php";
+
+$dotenv = Dotenv::createImmutable("../".__DIR__, "process.env");
+$dotenv -> load();
+
+namespace config\app\db;
+
+$dbserver = "localhost";
+$dbport = "9000";
+$dbname = "todolistdb";
 
 ?>
