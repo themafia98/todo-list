fee5beb6b32a8c316ed3cdaa63da448b2c547554
themafia98 <gaara33377@gmail.com>
2020-01-16 00:23:26 +0300

    Database model

--- PHP-ReactJS-todo/api/config/db.php
+++ PHP-ReactJS-todo/api/config/db.php
@@ -5,9 +5,18 @@
  */
 namespace config\db;
 
-$dbserver = "localhost";
-$dbport = "9000";
-$dbname = "todolistdb";
+use Dotenv\Dotenv;
+
+require "../vendor/autoload.php";
+
+$env = Dotenv::createImmutable("../".__DIR__, "process.env");
+
+
+$dbserver = getenv("DB_SERVER");
+$dbport = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$dbpassword = getenv("DB_PASSWORD");
+$dbuser = getenv("DB_USER");
 
 
 ?>
--- PHP-ReactJS-todo/api/core/models/Database.php
+++ PHP-ReactJS-todo/api/core/models/Database.php
@@ -5,12 +5,47 @@
  */
 namespace core\models\Database;
 
+use Error;
+use Exception;
+use mysqli;
 class Database 
 {
-    
-    public function __construct()
+
+    private $connect = null;
+
+    public function getConnect()
     {
-        
+        return $this -> connect;
+    }
+
+    public function setConnect(mysqli $cnt)
+    {
+        $this -> connect = $cnt;
+    }
+    
+    public function connection(string $host, string $user, string $password, string $db)
+    {
+        $connect = mysqli_connect($host, $user, $password, $db) or die(mysqli_error($connect));
+        $this -> setConnect($connect);
+    }
+
+    public function disconnection()
+    {
+        mysqli_close($this -> getConnect());
+        return true;
+    }
+
+    public function makeQuery($query)
+    {
+        try {
+            if (null === $this -> getConnect()) throw new Error ("Bad connect to bd"); 
+                $resultQuery = mysqli_query($this -> getConnect(), $query) or 
+                    die("Error:". mysqli_error($this -> getConnect()));
+            return $resultQuery;       
+            
+        } catch (Exception $error){
+            print_r($error);
+        }
     }
 }
 
