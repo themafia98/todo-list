fbf9f5a9ae879cd16abd797f357a2ae4d72a7095
themafia98 <gaara33377@gmail.com>
2020-02-12 21:59:07 +0300

    vendor path

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 /**
  * Bootstrap backend application
+ * Deployed ubuntu path /app/vendor/autoload.php
  */
 
-require  "../vendor/autoload.php";
+require "/app/vendor/autoload.php";
 
 
 
