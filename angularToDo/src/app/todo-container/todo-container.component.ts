5287da6b0377a03b1dd8da7c1b812a7a04fe348c
root <root@DESKTOP-G99F35S.localdomain>
2020-05-16 14:51:43 +0300

    test list

--- angularToDo/src/app/todo-container/todo-container.component.ts
+++ angularToDo/src/app/todo-container/todo-container.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface TodoItem {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'todo-container',
   templateUrl: './todo-container.component.html',
   styleUrls: ['./todo-container.component.scss']
 })
 export class TodoContainerComponent implements OnInit {
+  private itemsList: Array<TodoItem> = [{ id: 1, name: 'Hi' }, { id:2, name: 'Angular' }];
 
   constructor() { }
+
+  get items(){
+    return this.itemsList;
+  }
 
   ngOnInit(): void {
   }
