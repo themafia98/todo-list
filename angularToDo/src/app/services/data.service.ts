22a666a07a0131f41c39eddd3af4d66af1b2d911
root <root@DESKTOP-G99F35S.localdomain>
2020-05-18 02:45:31 +0300

    refactoring

--- angularToDo/src/app/services/data.service.ts
+++ angularToDo/src/app/services/data.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { TodoItem } from '../interface';
 import * as moment from 'moment';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export default class DataService {
 
   private todoList: Array<TodoItem>;
+  private observer: Observable<TodoItem[]>;
 
   constructor(private firestore: AngularFirestore){
     this.todoList = [];
-    this.firestore.collection<TodoItem>('todos').valueChanges()
-      .subscribe((items: Array<TodoItem>) => {
-        this.todoList = items.sort((a, b) => moment(b.date, "DD.MM.YYYY").unix() - moment(a.date, "DD.MM.YYYY").unix());
-      });
+    this.observer = this.firestore.collection<TodoItem>('todos').valueChanges();
+    this.init();
+  }
+
+  get obs(){
+    return this.observer;
   }
 
   get store(){
@@ -25,13 +29,22 @@ export default class DataService {
     return this.todoList;
   }
 
-  addItem(item: TodoItem){
-    this.firestore.collection('todos').add(item);
+  private init(): void {
+    this.obs.subscribe((items: Array<TodoItem>) => {
+      this.todoList = items.sort((a, b) => {
+        return moment(b.date, "DD.MM.YYYY").unix() - moment(a.date, "DD.MM.YYYY").unix()
+      });
+    },
+    (error: Error) => console.error(error));
   }
 
-  async deleteItem(id: string){
-    try{
+  public addItem(item: TodoItem){
+    this.firestore.collection('todos').add(item)
+    .catch(error => console.error(error));
+  }
 
+  public async deleteItem(id: string){
+    try{
       const result = await this.firestore.collection('todos').ref.where("id", "==", id).get();
       if (!result.docs.length)  throw new Error("no data for delete");
       for await (let doc of result.docs){
