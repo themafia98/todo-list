6f22c4ad00f0662044502ba5cd2d509c8cbdce52
root <root@DESKTOP-G99F35S.localdomain>
2020-05-18 22:55:01 +0300

    some styles and start edit

--- angularToDo/src/app/components/modal-window/editable-note/editable-note.component.ts
+++ angularToDo/src/app/components/modal-window/editable-note/editable-note.component.ts
@@ -6,8 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editable-note.component.scss']
 })
 export class EditableNoteComponent implements OnInit {
+  private name: string = 'click for add note';
+  private mode: string = 'view';
 
   constructor() { }
+
+  get content(){
+    return this.name;
+  }
+
+  set content(value: string){
+    this.name = value;
+  }
+
+  get modeView(){
+    return this.mode;
+  }
+
+  set modeView(mode: string){
+    this.mode = mode;
+  }
+
+  onSubmit(): void {
+    this.onSwitchMode('view');
+  }
+
+  onSwitchMode(mode: string): void {
+    if (mode === this.modeView) return;
+    this.modeView = mode;
+  }
 
   ngOnInit(): void {
   }
